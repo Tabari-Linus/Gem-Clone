28b66bbe50d2ad7e210eaf0863bef4a5fa222601
MrLii <leetamparega@gmail.com>
2024-09-12 10:15:59 +0000

    key in place

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,19 +3,21 @@
  *
  * $ npm install @google/generative-ai
  */
-
 const {
     GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,
   } = require("@google/generative-ai");
   
-  const apiKey = 'AIzaSyAGm4aIoBDTb1FpsTrXKf9jlkMY4wO3eFA';
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-  });
+const apiKey = process.env.REACT_APP_GOOGLE_GEMINI_APIKEY;
+
+if (!apiKey) {
+  throw new Error("Missing API Key");
+}
+const genAI = new GoogleGenerativeAI(apiKey);
+
+
+
   
   const generationConfig = {
     temperature: 1,
@@ -26,18 +28,24 @@ const {
   };
   
   async function runChat(prompt) {
+    const model = await genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+  
     const chatSession = model.startChat({
       generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
-      history: [
-      ],
+      // Optionally configure safety settings
+      // safetySettings: Adjust safety settings
+      // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [],
     });
   
     const result = await chatSession.sendMessage(prompt);
-    const resultText = result.response.text();
+    const resultText = await result.response.text(); // Await the text to be fully resolved
     console.log(resultText);
-    return resultText
+  
+    return resultText;
   }
   
   export default runChat;
+  
