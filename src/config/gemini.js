91d08fedbb8afa9fc23c4b11059bb19b320f3482
MrLii <leetamparega@gmail.com>
2024-09-12 02:25:17 +0000

    api linked to website

---
+++ src/config/gemini.js
@@ -0,0 +1,43 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const apiKey = 'AIzaSyAGm4aIoBDTb1FpsTrXKf9jlkMY4wO3eFA';
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function runChat(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const resultText = result.response.text();
+    console.log(resultText);
+    return resultText
+  }
+  
+  export default runChat;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App.js';
+import ContextProvider from './context/Context.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>
+  </ContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
